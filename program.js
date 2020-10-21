45d9749184f09be10a57a16a35cd329df2cc06fa
Peter Wolf <pewobox@Peters-Air.fritz.box>
2020-10-21 14:58:54 +0200

    added index with button and field, program.js included variables list

---
+++ program.js
@@ -0,0 +1,10 @@
+
+// input field
+//variables list
+let choosePrio = document.getElementById("prioSelector");
+let addNewItem = document.getElementById("addButton");
+let chooseDate = document.getElementById("calendar");
+
+console.log(choosePrio.length);
+console.log(chooseDate);
+console.log(addNewItem);
