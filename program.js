ea82370cbfd28fec86740df9068633b4c738175a
Peter Wolf <pewobox@Peters-Air.fritz.box>
2020-10-22 17:16:30 +0200

    new commitment peter

--- program.js
+++ program.js
@@ -8,3 +8,17 @@ let chooseDate = document.getElementById("calendar");
 console.log(choosePrio.length);
 console.log(chooseDate);
 console.log(addNewItem);
+
+
+//add new item
+
+let toDoList = document.getElementById("list")
+
+addNewItem.addEventListener("click", function() {
+    let listItem = toDoList.document.createElement("<li>");
+    listItem.appendChild(toDoList);
+
+})
+
+
+
